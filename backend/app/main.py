8f021d75bc1e209b793a31d2284b952dec3be5a2
Vidushi Bhadauria <vidushibhadauria06@gmail.com>
2026-06-20 01:00:54 +0530

    Update main.py

--- backend/app/main.py
+++ backend/app/main.py
@@ -69,7 +69,7 @@ allowed_origins = os.getenv(
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=origins,
+    allow_origins=[o.strip() for o in allowed_origins],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
